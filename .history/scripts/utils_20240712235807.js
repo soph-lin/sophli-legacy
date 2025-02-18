0bdf745529f9f0e4a3c31314342fa1ed73b9deee
soph-lin <slin12321@gmail.com>
2025-02-17 22:33:55 -0600

    Remove history

--- .history/scripts/utils_20240712235807.js
+++
@@ -1,8 +0,0 @@
-function restartAnim(obj, anim) {
-    if (obj) {
-        obj.classList.remove(anim);
-        void obj.offsetWidth; // Trigger a reflow. Ensures animation starts at beginning.
-        obj.classList.add(anim);
-    }
-    else console.error('Object not found for restartAnim');
-}
